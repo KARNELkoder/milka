e815421287c9c97b0a4f02aabc42f45f283fe176
KARNELkoder <106372780+KARNELkoder@users.noreply.github.com>
2022-08-25 23:31:08 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,8 +3,8 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "05.06.2004"; // Дата рождения
-var fullname = "Гричковский Владислав Владимирович" // ФИО
+var birthdate = "02.05.2004"; // Дата рождения
+var fullname = "Иванов Милан Николаевич" // ФИО
 var name = "Имя" // Имя
 
 // Паспорт
